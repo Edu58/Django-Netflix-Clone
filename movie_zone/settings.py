9b7d6f60ebbcabe8dbdc1d86d8e057d9a0f70269
“Edu58” <edumuriithi58@gmail.com>
2022-06-01 16:40:01 +0300

    add requests lib and env variables

--- movie_zone/settings.py
+++ movie_zone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h=9)i40(ff469c34&-_uvw^o_!g-3i=zrk1#ux_a#2!(&k$58&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +80,9 @@ WSGI_APPLICATION = 'movie_zone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD')
     }
 }
 
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .apis import get_movie
 
 
 # Create your views here.
 def home(request):
+    movie = get_movie()
+    print(movie)
     return render(request, 'index.html')
