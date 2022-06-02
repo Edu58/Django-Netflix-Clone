2f1719081f2b0d81d420ce1dd994a867c5bee7a2
“Edu58” <edumuriithi58@gmail.com>
2022-06-02 14:36:37 +0300

    adds search bar

--- movies/views.py
+++ movies/views.py
@@ -7,38 +7,38 @@ import random
 def home(request):
     # banner and trending
     movie = get_trending()
-    random_movie = random.randrange(0, 11)
+    random_movie = random.randrange(0, 21)
     banner = movie['results'][random_movie]
     trending = movie['results']
-    # top_rated
-    top_rated = get_top_rated()
-
-    # action movies
-    action = get_genre(28)
-
-    # comedy movies
-    comedy = get_genre(35)
-
-    # horror movies
-    crime = get_genre(80)
-
-    # family movies
-    fantasy = get_genre(14)
-
-    # documentary movies
-    documentary = get_genre(99)
+    # # top_rated
+    # top_rated = get_top_rated()
+    #
+    # # action movies
+    # action = get_genre(28)
+    #
+    # # comedy movies
+    # comedy = get_genre(35)
+    #
+    # # horror movies
+    # crime = get_genre(80)
+    #
+    # # family movies
+    # fantasy = get_genre(14)
+    #
+    # # documentary movies
+    # documentary = get_genre(99)
 
     return render(request,
                   'index.html',
                   {
                       'banner': banner,
                       'trending': trending,
-                      'top_rated': top_rated['results'],
-                      'action': action['items'],
-                      'comedy': comedy['items'],
-                      'crime': crime['items'],
-                      'fantasy': fantasy['items'],
-                      'documentary': documentary['items'],
+                      # 'top_rated': top_rated['results'],
+                      # 'action': action['items'],
+                      # 'comedy': comedy['items'],
+                      # 'crime': crime['items'],
+                      # 'fantasy': fantasy['items'],
+                      # 'documentary': documentary['items'],
                   })
 
 
