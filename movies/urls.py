9fe007ee4cc7c4cdce3f87547e3eafb2c413d312
“Edu58” <edumuriithi58@gmail.com>
2022-06-03 16:21:24 +0300

    adds authentication

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('play/<movie_id>', views.play, name='play')
+    path('play/<movie_id>', views.play, name='play'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('search/', views.search, name='search'),
+    path('profile/', views.profilePage, name='profile'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,60 @@
-from django.shortcuts import render
-from .apis import get_movie, get_trending, get_top_rated, get_genre, get_video
+import imp
+from urllib import response
+from django.shortcuts import redirect, render
+from .apis import get_movie, get_trending, get_top_rated, get_genre, get_video, searchMulti
 import random
+from .forms import CreateUserForm, LoginUserForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+def registerPage(request):
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+    
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('login')
+            
+    return render(request, 'register.html', {'form': form})
 
 
-# Create your views here.
+def loginPage(request):
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = LoginUserForm()
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        
+        user  = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required(login_url='login')
+def profilePage(request):
+    return render(request, 'profile.html')
+
+
+@login_required(login_url='login')
 def home(request):
     # banner and trending
     movie = get_trending()
-    random_movie = random.randrange(0, 21)
+    random_movie = random.randrange(0, 18)
     banner = movie['results'][random_movie]
     trending = movie['results']
     # # top_rated
@@ -42,9 +89,32 @@ def home(request):
                   })
 
 
+@login_required(login_url='login')
 def play(request, movie_id):
     data = get_video(movie_id)
     video_info = data['results'][0]
     youtube_url = f"www.youtube.com/watch?v={video_info['key']}"
 
     return render(request, 'play.html', {'url': youtube_url})
+
+
+@login_required(login_url='login')
+def search(request):
+    
+    if request.method == "POST":
+        term = request.POST.get('term')
+        
+        response = searchMulti(term)
+    
+        results = response['results']
+        
+        if len(results) > 0 :
+            return render(request, 'search.html', {'results': results})
+
+    return render(request, 'search.html')
+
+
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
