88476f078242e4ab9690bf615f0d16fa65787a40
“Edu58” <edumuriithi58@gmail.com>
2022-06-03 17:14:49 +0300

    update requirements.txt

--- movies/views.py
+++ movies/views.py
@@ -59,35 +59,35 @@ def home(request):
     random_movie = random.randrange(0, 18)
     banner = movie['results'][random_movie]
     trending = movie['results']
-    # # top_rated
-    # top_rated = get_top_rated()
-    #
-    # # action movies
-    # action = get_genre(28)
-    #
-    # # comedy movies
-    # comedy = get_genre(35)
-    #
-    # # horror movies
-    # crime = get_genre(80)
-    #
-    # # family movies
-    # fantasy = get_genre(14)
-    #
-    # # documentary movies
-    # documentary = get_genre(99)
+    # top_rated
+    top_rated = get_top_rated()
+    
+    # action movies
+    action = get_genre(28)
+    
+    # comedy movies
+    comedy = get_genre(35)
+    
+    # horror movies
+    crime = get_genre(80)
+    
+    # family movies
+    fantasy = get_genre(14)
+    
+    # documentary movies
+    documentary = get_genre(99)
 
     return render(request,
                   'index.html',
                   {
                       'banner': banner,
                       'trending': trending,
-                      # 'top_rated': top_rated['results'],
-                      # 'action': action['items'],
-                      # 'comedy': comedy['items'],
-                      # 'crime': crime['items'],
-                      # 'fantasy': fantasy['items'],
-                      # 'documentary': documentary['items'],
+                      'top_rated': top_rated['results'],
+                      'action': action['items'],
+                      'comedy': comedy['items'],
+                      'crime': crime['items'],
+                      'fantasy': fantasy['items'],
+                      'documentary': documentary['items'],
                   })
 
 
